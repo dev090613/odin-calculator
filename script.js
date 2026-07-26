d65959e03046f3c8024c6d7be9bd31f4998a1815
dev090613 <128305501+dev090613@users.noreply.github.com>
2026-07-26 09:55:10 +0900

    add basic functions

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log(add(1, 2))
+console.log(subtract(1, 2))
+console.log(multiply(2, 0))
+console.log(divide(1, 3))
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  return num1 / num2
+}

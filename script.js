5704a921525ddafe03455ffca332b0d65f946589
dev090613 <128305501+dev090613@users.noreply.github.com>
2026-07-27 14:03:11 +0900

    create simple ui skeleton

--- script.js
+++ script.js
@@ -3,6 +3,30 @@ console.log(subtract(1, 2))
 console.log(multiply(2, 0))
 console.log(divide(1, 3))
 
+const OP = '';
+const num1 = '';
+const num2 = '';
+
+function operate(OP, num1, num2) {
+  let result = ''
+
+  switch (OP) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "%":
+      result = divide(num1, num2);
+      break;
+  }
+  return result;
+}
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -18,3 +42,6 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2
 }
+
+const yearSpan = document.querySelector('#year')
+yearSpan.textContent = new Date().getFullYear() + ' JunsooLee'
